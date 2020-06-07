ffff72c608229921f2f9bd7620443386db03eda3
Inês Martins <in.afmartins@gmail.com>
2020-06-07 17:59:04 +0100

    removes old docs and adds ghost

--- index.js
+++
@@ -1,8 +0,0 @@
-function toggleVisibility(id) {
-    var elm = document.getElementById(id);
-    if (elm.style.display === "none") {
-        elm.style.display = "block";
-    } else {
-        elm.style.display = "none";
-    }
-}
