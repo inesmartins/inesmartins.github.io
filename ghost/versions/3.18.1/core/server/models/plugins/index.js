ea62940e6760883c8f0fbfe937f0749c5e6ac86e
Inês Martins <in.afmartins@gmail.com>
2020-06-07 23:10:27 +0100

    adds static content

--- ghost/versions/3.18.1/core/server/models/plugins/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    filter: require('./filter'),
-    search: require('./search'),
-    includeCount: require('./include-count'),
-    pagination: require('./pagination'),
-    collision: require('./collision'),
-    transactionEvents: require('./transaction-events'),
-    hasPosts: require('./has-posts')
-};
